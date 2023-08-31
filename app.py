bf380a11264644a55193488167b0cca466e5eb07
Eleven <sline@ucsc.edu>
2023-08-30 17:30:14 -0700

    website v2

--- app.py
+++ app.py
@@ -3,19 +3,28 @@ import stripe
 import os
 app = Flask(__name__, static_url_path= "", static_folder="public")
 
-stripe.api_key = os.environ.get('STRIPE_SECRET_KEY')
+test_price = 'price_1NkxgQHiDjhflRKUYpjQ981z'
+recur_test_price = 'price_1NkxlUHiDjhflRKUbOY9YThZ'
+live_price = 'price_1N4C4rHiDjhflRKUQtRNAGAz'
+recur_live_price = 'price_1N4CcAHiDjhflRKUmVIdrS7l'
+
+
+stripe.api_key = 'sk_test_51N4BDbHiDjhflRKUHquvU8LYLFJCyhfvMn6LzQP0Z7vRVGsebsW9647AKj24Jrp8175s6BjCfP1KAMZt2gXPjvg900NybbpdFP'#os.environ.get('STRIPE_SECRET_KEY')
 YOUR_DOMAIN = "http://127.0.0.1:5000"
 
+@app.route('/')
+def home():
+	return app.send_static_file('index.html')
 
 @app.route('/create-checkout-session',methods=['POST'])
 def create_checkout_session():
 	payment_type = request.form.get('payment_type')
 	try:
 		if payment_type == "one_time":
-			price_id = 'price_1N4C4rHiDjhflRKUQtRNAGAz'
+			price_id = test_price
 			mode = "payment"
 		elif payment_type == "monthly":
-			price_id = 'price_1N4CcAHiDjhflRKUmVIdrS7l'
+			price_id = recur_test_price
 			mode = "subscription"
 		else:
 			return "Invalid payment type", 400
@@ -29,7 +38,7 @@ def create_checkout_session():
 			],
 			mode = mode,
 			success_url= YOUR_DOMAIN + "/success.html",
-			cancel_url = YOUR_DOMAIN + "/cancel.html"
+			cancel_url = YOUR_DOMAIN + "/index.html"
 		)
 	except Exception as e:
 		return str(e)
@@ -37,5 +46,5 @@ def create_checkout_session():
 
 	return redirect(checkout_session.url, code = 303)
 if __name__ == "__main__":
-	app.run(host = "0.0.0.0",port=5000, debug=True)
+	app.run(debug = True)
 	
